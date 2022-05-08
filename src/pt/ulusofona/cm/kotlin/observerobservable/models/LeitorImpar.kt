e9be032faf9eb1ac07f5aef40336c1612c3c6970
filipe-21903016 <a21903016@alunos.ulht.pt>
2022-05-09 00:13:25 +0100

    fixed typo on Leitorimpar

--- src/pt/ulusofona/cm/kotlin/observerobservable/models/LeitorImpar.kt
+++ src/pt/ulusofona/cm/kotlin/observerobservable/models/LeitorImpar.kt
@@ -12,6 +12,6 @@ class LeitorImpar(nome: String = "LeitorImpar") : Leitor(nome), OnNumeroListener
     fun imprimeNumeros() : String{
         val numsStr : StringBuilder = StringBuilder()
         numeros.forEach { numsStr.append(it) }
-        return "$nome leu os seguintes numeros pares: $numsStr"
+        return "$nome leu os seguintes numeros impares: $numsStr"
     }
 }
